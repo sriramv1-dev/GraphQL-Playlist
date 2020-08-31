0980cbb43cd1a2f0cc5a838871262827f8f9af48
Sriram Voonna <sriram.voonna@mantechapps.com>
2020-08-30 23:59:50 -0400

    client and server
    react app added to the playlist

---
+++ server/securityConstants.js
@@ -0,0 +1,7 @@
+// remove the file from git
+
+const connectionString = `mongodb+srv://ramv2:ramv2@firstcluster.bue02.mongodb.net/gql-books-authors?retryWrites=true&w=majority`;
+
+module.exports = {
+  connectionString,
+};
